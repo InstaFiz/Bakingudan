713b1d911737e28604bb2ecb5847f6639207dfa1
InstaFiz <instafiz0@gmail.com>
2025-03-07 19:12:54 -0600

    better platform engine

--- Assets/scripts/TankPlayerController.cs
+++ Assets/scripts/TankPlayerController.cs
@@ -1,54 +1,49 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankPlayerController : MonoBehaviour //a monobehavior is a script that can be attached
+public class TankPlayerController : MonoBehaviour
 {
-    public float speed;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float verticalInput;
+	//try setting this to 8 in the inspector
+	public float speed;
 
+	//try setting this to 100 in the inspector
+	public float turnSpeed;
 
-  
-    // Update is called once per frame
-    void Update()
-    {
+	public float horizontalInput;
+	public float verticalInput;
 
-        //Move Forward        x, y, z   axis
-        //transform.Translate(1, 0, 0);  //this will send the box flying to the right
-        
-        //which is the same as 
-    //    transform.Translate(Vector2.right);  //going one to the right on the x axis
+	// Update is called once per frame
+	
+	void Update()
+	{
+		//Move forward
+		//transform.Translate(1, 0);
 
-        //we can low this speed  down with delta time speed goes by how fast the computer is going so we scale by 
-        //move to the right at 20m/s if speed is set to 20
-       // transform.Translate(Vector2.right * Time.deltaTime * speed); //by adding speed we can change the speed in unity since out float vars go there
+		//Which is the same as...
+		// transform.Translate(Vector2.right);
 
-        //Get input from user - do this in Update()
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+		//Move forward 20meters/second if speed is set to 20
+		//transform.Translate(Vector2.right * Time.deltaTime * speed);
 
-        // move the player forward with vertical input
-        transform.Translate(Vector2.right * speed * Time.deltaTime * verticalInput);
+		//Get Input - Do this in Update()
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
 
-        //rotate player with horizontal input
-        // transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * horizontalInput);
-        /////////////////axis were rotating about,   float angel
+		//Move player side-to-side with horizontal Input
+		//transform.Translate(Vector2.right * turnSpeed * Time.deltaTime* horizontalInput);
 
+		//Move player forward with vertical Input
+		transform.Translate(Vector2.right * Time.deltaTime * speed * verticalInput);
 
-        //rotate palyer with the horizontal input
-        //but reverse the orientation direction when moving backwards
-        if (verticalInput < 0)
-        {
-            transform.Rotate(Vector3.back, -turnSpeed * Time.deltaTime * horizontalInput);
-        }
-        else {
-            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime * horizontalInput);
-        }
+		//Rotate player with horizontal Input
+		//transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime * horizontalInput);
 
-        //Move player side-to-side with horizontal input
-        //transform.Translate(Vector2.right * turnSpeed * Time.deltaTime * horizontalInput);
-    }
+		//Rotate player with horizontal Input
+		//but reverse the rotation direction when moving backwards
+		if(verticalInput < 0)
+		    transform.Rotate(Vector3.back, -turnSpeed * Time.deltaTime * horizontalInput);
+		else
+		    transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime * horizontalInput);
+	}
 }
