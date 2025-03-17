8a902f7a365aa3283a2bdc827958446d88edbe65
InstaFiz <instafiz0@gmail.com>
2025-03-16 23:56:53 -0500

    Revert "Added UI to display King's health"
    This reverts commit 1cb37570f738304045ec7c60065002ddda9b2ee9.

--- Assets/Scripts/DischargeDamage.cs
+++ Assets/Scripts/DischargeDamage.cs
@@ -27,7 +27,6 @@ public class DischargeDamage : MonoBehaviour
         }
     }
 
-
     // Start is called before the first frame update
     void Start()
     {
--- Assets/scripts/PlatformerPlayerController.cs
+++ Assets/scripts/PlatformerPlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // Import TextMeshPro namespace
 
 public class PlatformerPlayerController : MonoBehaviour
 {
@@ -20,78 +19,43 @@ public class PlatformerPlayerController : MonoBehaviour
 
     public bool theKing = false;
     public GameObject crownVanity;
-    public GameObject crownPrefab;
 
-    public int health = 15;
-    public int maxHealth = 15;
+    public int health = 400;
 
-    public TextMeshProUGUI scoreText; // UI reference for score display
-
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        if (GroundCheck == null)
-            Debug.LogError("GroundCheck not assigned to the player controller!");
-
-        UpdateScoreUI(); // Initialize the score UI at the start
+	if (GroundCheck == null)
+	    Debug.LogError("GroundCheck not assigned to the player controller!");
     }
 
+    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis(horizontalNum);
 
-        if (Input.GetButtonDown(jumpNum) && isGrounded)
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+	    if (Input.GetButtonDown(jumpNum) && isGrounded)
+	        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
         crownVanity.GetComponent<Renderer>().enabled = theKing;
 
-        if (health <= 0 && theKing)
+        if (health <= 0)
         {
-            DropCrown();
+            ScoreManager.gameOver = true;
+            Object.Destroy(gameObject);
         }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayer);
+	isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayer);
 
-        if (horizontalInput > 0)
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        else if (horizontalInput < 0)
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+	if (horizontalInput > 0)
+	    transform.localScale = new Vector3(1f, 1f, 1f);
+	else if (horizontalInput < 0)
+	    transform.localScale = new Vector3(-1f, 1f, 1f);
     }
-
-    public void TakeDamage(int damage)
-    {
-        if (theKing)
-        {
-            health -= damage;
-            UpdateScoreUI(); // Update the UI when health changes
-
-            if (health <= 0)
-            {
-                DropCrown();
-            }
-        }
-    }
-
-    void DropCrown()
-    {
-        theKing = false;
-        health = maxHealth;
-        UpdateScoreUI(); // Update UI when the crown is lost
-
-        Instantiate(crownPrefab, transform.position, Quaternion.identity);
-    }
-
-    public void UpdateScoreUI()
-    {
-        if (scoreText != null)
-        {
-            scoreText.text = "King's Health: " + health;
-        }
-    }
-
 }
